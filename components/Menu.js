4e6f32b8e2719ccabb269bed916c68ba1e1e083b
sok-leaphea <leapheas2@gmail.com>
2024-07-25 15:12:10 +0700

    about_us_screen

--- components/Menu.js
+++ components/Menu.js
@@ -1,65 +1,74 @@
-import React from "react";
-import { ScrollView, StyleSheet } from "react-native";
+import React from 'react';
+import { ScrollView, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import MenuItem from "./MenuItems";
 
 const menuItems = [
   {
-    id: "1",
-    iconName: "map",
-    name: "Map",
+    id: '1',
+    iconName: 'map',
+    name: 'Map',
     iconSize: 48,
-    iconColor: "green",
-    boxStyle: { backgroundColor: "white" },
-    textStyle: { color: "black" },
+    iconColor: 'green',
+    boxStyle: { backgroundColor: 'white' },
+    textStyle: { color: 'black' },
+    screen: 'Map',
   },
   {
-    id: "2",
-    iconName: "trash-bin",
-    name: "Bins",
+    id: '2',
+    iconName: 'trash-bin',
+    name: 'Bins',
     iconSize: 48,
-    iconColor: "green",
-    boxStyle: { backgroundColor: "white" },
-    textStyle: { color: "black" },
+    iconColor: 'green',
+    boxStyle: { backgroundColor: 'white' },
+    textStyle: { color: 'black' },
+    screen: 'Bins',
   },
   {
-    id: "3",
-    iconName: "",
-    name: "About us",
+    id: '3',
+    iconName: 'people',
+    name: 'About us',
     iconSize: 48,
-    iconColor: "green",
-    boxStyle: { backgroundColor: "white" },
-    textStyle: { color: "black" },
+    iconColor: 'green',
+    boxStyle: { backgroundColor: 'white' },
+    textStyle: { color: 'black' },
+    screen: 'AboutUs',
   },
   {
-    id: "4",
-    iconName: "settings",
-    name: "Settings",
+    id: '4',
+    iconName: 'settings',
+    name: 'Settings',
     iconSize: 48,
-    iconColor: "green",
-    boxStyle: { backgroundColor: "white" },
-    textStyle: { color: "black" },
+    iconColor: 'green',
+    boxStyle: { backgroundColor: 'white' },
+    textStyle: { color: 'black' },
+    screen: 'Settings',
   },
   {
-    id: "5",
-    iconName: "settings",
-    name: "Settings",
+    id: '5',
+    iconName: 'settings',
+    name: 'Settings',
     iconSize: 48,
-    iconColor: "green",
-    boxStyle: { backgroundColor: "white" },
-    textStyle: { color: "black" },
+    iconColor: 'green',
+    boxStyle: { backgroundColor: 'white' },
+    textStyle: { color: 'black' },
+    screen: 'Settings',
   },
   {
-    id: "6",
-    iconName: "home",
-    name: "Home",
+    id: '6',
+    iconName: 'home',
+    name: 'Home',
     iconSize: 48,
-    iconColor: "green",
-    boxStyle: { backgroundColor: "white" },
-    textStyle: { color: "black" },
+    iconColor: 'green',
+    boxStyle: { backgroundColor: 'white' },
+    textStyle: { color: 'black' },
+    screen: 'Home',
   },
 ];
 
 const Menu = () => {
+  const navigation = useNavigation();
+
   return (
     <ScrollView contentContainerStyle={styles.container}>
       {menuItems.map((item) => (
@@ -71,6 +80,7 @@ const Menu = () => {
           name={item.name}
           boxStyle={item.boxStyle}
           textStyle={item.textStyle}
+          action={() => navigation.navigate(item.screen)}
         />
       ))}
     </ScrollView>

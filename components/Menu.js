7ceb1a1466617c1f0bd127cf1d62ef70ad54ddd0
chhinsatya <>
2024-07-25 23:39:09 +0700

    fix: FlatList and baner

--- components/Menu.js
+++ components/Menu.js
@@ -1,101 +1,140 @@
-import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import MenuItem from "./MenuItems";
+import React from "react";
+import { FlatList, StyleSheet, View, Image } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import MenuItems from "./MenuItems";
+import { Ionicons } from "@expo/vector-icons";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import { MaterialIcons } from "@expo/vector-icons";
+import { FontAwesome6 } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
 
 const menuItems = [
   {
-    id: '1',
-    iconName: 'map',
-    name: 'Map',
+    id: "1",
+    name: "Map",
+    iconName: "map",
     iconSize: 48,
-    iconColor: 'green',
-    boxStyle: { backgroundColor: 'white' },
-    textStyle: { color: 'black' },
-    screen: 'Map',
+    iconColor: "green",
+    IconComponent: Entypo,
+    boxStyle: { backgroundColor: "white" },
+    textStyle: { color: "black" },
+    screen: "AboutUs",
   },
   {
-    id: '2',
-    iconName: 'trash-bin',
-    name: 'Bins',
+    id: "2",
+    iconName: "trash-alt",
+    name: "Bins",
     iconSize: 48,
-    iconColor: 'green',
-    boxStyle: { backgroundColor: 'white' },
-    textStyle: { color: 'black' },
-    screen: 'Bins',
+    iconColor: "green",
+    IconComponent: FontAwesome5,
+    boxStyle: { backgroundColor: "white" },
+    textStyle: { color: "black" },
+    screen: "AboutUs",
   },
   {
-    id: '3',
-    iconName: 'people',
-    name: 'About us',
+    id: "3",
+    iconName: "schedule",
+    name: "Schedule",
     iconSize: 48,
-    iconColor: 'green',
-    boxStyle: { backgroundColor: 'white' },
-    textStyle: { color: 'black' },
-    screen: 'AboutUs',
+    iconColor: "green",
+    IconComponent: MaterialIcons,
+    boxStyle: { backgroundColor: "white" },
+    textStyle: { color: "black" },
+    screen: "AboutUs",
+  },
+
+  {
+    id: "4",
+    iconName: "database",
+    name: "Data",
+    iconSize: 48,
+    iconColor: "green",
+    IconComponent: AntDesign,
+    boxStyle: { backgroundColor: "white" },
+    textStyle: { color: "black" },
+    screen: "AboutUs",
   },
   {
-    id: '4',
-    iconName: 'settings',
-    name: 'Settings',
+    id: "5",
+    iconName: "information-circle-outline",
+    name: "About us",
     iconSize: 48,
-    iconColor: 'green',
-    boxStyle: { backgroundColor: 'white' },
-    textStyle: { color: 'black' },
-    screen: 'Settings',
+    iconColor: "green",
+    IconComponent: Ionicons,
+    boxStyle: { backgroundColor: "white" },
+    textStyle: { color: "black" },
+    screen: "AboutUs",
   },
   {
-    id: '5',
-    iconName: 'settings',
-    name: 'Settings',
+    id: "6",
+    iconName: "circle-user",
+    name: "Profile",
     iconSize: 48,
-    iconColor: 'green',
-    boxStyle: { backgroundColor: 'white' },
-    textStyle: { color: 'black' },
-    screen: 'Settings',
-  },
-  {
-    id: '6',
-    iconName: 'home',
-    name: 'Home',
-    iconSize: 48,
-    iconColor: 'green',
-    boxStyle: { backgroundColor: 'white' },
-    textStyle: { color: 'black' },
-    screen: 'Home',
+    iconColor: "green",
+    IconComponent: FontAwesome6,
+    boxStyle: { backgroundColor: "white" },
+    textStyle: { color: "black" },
+    screen: "AboutUs",
   },
 ];
 
 const Menu = () => {
   const navigation = useNavigation();
 
+  const renderItem = ({ item }) => (
+    <MenuItems
+      IconComponent={item.IconComponent}
+      iconName={item.iconName}
+      iconSize={item.iconSize}
+      iconColor={item.iconColor}
+      name={item.name}
+      boxStyle={item.boxStyle}
+      textStyle={item.textStyle}
+      action={() => navigation.navigate(item.screen)}
+    />
+  );
+
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      {menuItems.map((item) => (
-        <MenuItem
-          key={item.id}
-          iconName={item.iconName}
-          iconSize={item.iconSize}
-          iconColor={item.iconColor}
-          name={item.name}
-          boxStyle={item.boxStyle}
-          textStyle={item.textStyle}
-          action={() => navigation.navigate(item.screen)}
+    <View>
+      <View style={styles.imageContainer}>
+        <Image
+          source={require("../assets/logo/baner-bin.png")}
+          style={styles.image}
         />
-      ))}
-    </ScrollView>
+      </View>
+      <FlatList
+        data={menuItems}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        numColumns={3}
+        contentContainerStyle={styles.container}
+        columnWrapperStyle={styles.columnWrapper}
+      />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    padding: 10,
-    display: "grid",
-    flexWrap: "wrap",
-    justifyContent: "center",
-    gap: 16,
-    alignItems: "center",
-    flexDirection: "row",
+    padding: 16,
+  },
+  columnWrapper: {
+    justifyContent: "space-between",
+  },
+  imageContainer: {
+    width: "100%",
+    height: "50%",
+    resizeMode: "contain",
+    alignSelf: "center",
+    paddingTop: 16,
+    paddingHorizontal: 16,
+    overflow: "hidden",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 10,
   },
 });
 

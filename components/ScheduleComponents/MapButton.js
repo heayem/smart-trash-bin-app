37d7c9b00ba04f2b6878eea133325a84d73eddb0
sok-leaphea <leapheas2@gmail.com>
2024-08-18 08:11:59 +0700

    fix: go to map button

--- components/ScheduleComponents/MapButton.js
+++ components/ScheduleComponents/MapButton.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { View, Button, StyleSheet } from "react-native";
 
-const Mapbutton = ({ navigation }) => (
+const MapButton = ({ navigation, title = "Go to Map", onPressAction }) => (
   <View style={styles.buttonContainer}>
     <Button
-      title="Go to Map"
-      onPress={() => navigation.navigate("Map")}
+      title={title}
+      onPress={() => {
+        if (onPressAction) {
+          onPressAction();
+        } else {
+          navigation.navigate("Map", { triggerCalculateRoutes: true });
+        }
+      }}
       color="#007AFF"
     />
   </View>
@@ -15,7 +21,8 @@ const styles = StyleSheet.create({
   buttonContainer: {
     borderRadius: 10,
     overflow: "hidden",
+    margin: 10, // added margin for better spacing
   },
 });
 
-export default Mapbutton;
+export default MapButton;

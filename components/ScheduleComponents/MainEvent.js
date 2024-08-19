50bb2d8a61500e01d3705d3acec884fe06d28920
chhinsatya <>
2024-08-19 21:24:16 +0700

    fix: styling event

--- components/ScheduleComponents/MainEvent.js
+++ components/ScheduleComponents/MainEvent.js
@@ -2,7 +2,7 @@ import React from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { formatTimeString } from "../../Helper/TimeConvert";
-import Mapbutton from "./MapButton"; 
+import Mapbutton from "./MapButton";
 
 const MainEvent = ({ event, navigation, removeEvent }) => (
   <View style={styles.scheduleContainer}>
@@ -13,10 +13,7 @@ const MainEvent = ({ event, navigation, removeEvent }) => (
       Check the map for detailed routes and bins to collect.
     </Text>
     <Mapbutton navigation={navigation} />
-    <TouchableOpacity
-      style={styles.removeButton}
-      onPress={removeEvent}
-    >
+    <TouchableOpacity style={styles.removeButton} onPress={removeEvent}>
       <Ionicons name="remove-circle-sharp" size={18} color="#ffb2a2" />
     </TouchableOpacity>
   </View>
@@ -24,11 +21,12 @@ const MainEvent = ({ event, navigation, removeEvent }) => (
 
 const styles = StyleSheet.create({
   scheduleContainer: {
-    marginBottom: 20,
-    padding: 15,
+    padding: 16,
+    gap: 12,
+    marginVertical: 12,
     backgroundColor: "#fff",
-    borderRadius: 10,
-    shadowColor: "#000",
+    borderRadius: 12,
+    shadowColor: "#eee",
     shadowOpacity: 0.1,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 10,
@@ -37,23 +35,19 @@ const styles = StyleSheet.create({
   eventTitle: {
     fontSize: 20,
     fontWeight: "bold",
-    marginBottom: 10,
     color: "#007AFF",
   },
   eventDescription: {
     fontSize: 16,
     color: "#666",
-    marginBottom: 10,
   },
   eventTime: {
     fontSize: 16,
     color: "#333",
-    marginBottom: 10,
   },
   mapDescription: {
     fontSize: 16,
     color: "#666",
-    marginBottom: 10,
   },
   buttonContainer: {
     borderRadius: 10,
@@ -61,10 +55,9 @@ const styles = StyleSheet.create({
   },
   removeButton: {
     position: "absolute",
-    right: 5,
-    top: 5,
+    right: 8,
+    top: 8,
   },
 });
-
 
 export default MainEvent;
